3b94381fdb1ea5cacf5d62298a9ec8c8b570c908
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2024-12-23 22:26:36 +0100

    Reformatted the code using ruff

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from plot_path import plot_path
 from random_walk import generate_random_walk
 
+
 def main():
     print("Hello from example-uv!")
     print("Generating random walk...")
+
     walk = generate_random_walk(10000)
     print("Plotting walk...")
+
     plot_path(walk)
 
 
